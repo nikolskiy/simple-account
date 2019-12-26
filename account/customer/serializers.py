e09a08a2610e501ccbcf508e8dbc18dc084ee999
Denis <fsdvn1@gmail.com>
2019-12-26 07:43:39 -0800

    ping, add, subtract, and status functionality was added.

--- account/customer/serializers.py
+++ account/customer/serializers.py
@@ -6,3 +6,11 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('uuid', 'name', 'balance', 'hold', 'status')
+
+
+class InfoSerializer(serializers.Serializer):
+    uuid = serializers.UUIDField(format='hex_verbose')
+
+
+class UpdateSerializer(InfoSerializer):
+    amount = serializers.DecimalField(max_digits=8, decimal_places=2)
--- account/customer/views.py
+++ account/customer/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, views
+from collections import OrderedDict
+from decimal import Decimal
+from rest_framework import viewsets, generics, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Customer
-from .serializers import CustomerSerializer
+from .serializers import CustomerSerializer, UpdateSerializer, InfoSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
@@ -11,33 +13,101 @@ class CustomerViewSet(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
 
 
-class AddView(views.APIView):
-    def get(self, request, format=None):
-        return Response()
+class AccountMixin:
+    serializer_class = UpdateSerializer
+    customer_serializer_class = CustomerSerializer
 
-    def post(self, request, format=None):
-        return Response()
+    def transaction(self, obj, data, resp):
+        resp['status'] = status.HTTP_200_OK
+        resp['result'] = True
+        customer_serializer = self.customer_serializer_class(instance=obj)
+        resp['addition'] = customer_serializer.data
+        return Response(resp, status=resp['status'])
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        return self.build_response(serializer)
+
+    def build_response(self, serializer):
+        resp = OrderedDict([
+            # If this was not replaced something went wrong.
+            ('status', status.HTTP_500_INTERNAL_SERVER_ERROR),
+            ('result', False),
+            ('addition', {}),
+            ('description', {})
+        ])
+
+        # make sure we got valid request from the user
+        if not serializer.is_valid():
+            resp['status'] = status.HTTP_400_BAD_REQUEST
+            resp['addition'] = {}
+            resp['description'] = {'errors': serializer.errors}
+            return Response(resp, status=resp['status'])
+        obj = Customer.objects.filter(uuid=serializer.data['uuid']).first()
+        if not obj:
+            resp['status'] = status.HTTP_400_BAD_REQUEST
+            resp['addition'] = {}
+            resp['description'] = [
+                'Customer with {} uuid does not exist.'.format(
+                    serializer.data['uuid']
+                )
+            ]
+            return Response(resp, status=resp['status'])
+
+        return self.transaction(obj, serializer.data, resp)
 
 
-class SubtractView(views.APIView):
-    def get(self, request, format=None):
-        return Response()
+class AddView(AccountMixin, generics.GenericAPIView):
+    """Increase balance"""
 
-    def post(self, request, format=None):
-        return Response()
+    def transaction(self, instance, data, resp):
+        amount = Decimal(data['amount'])
+        instance.balance += amount
+        instance.save()
+        resp['status'] = status.HTTP_200_OK
+        resp['result'] = True
+        resp['addition'] = self.customer_serializer_class(instance=instance).data
+        return Response(resp, status=resp['status'])
 
 
-class StatusView(views.APIView):
-    def get(self, request, format=None):
-        return Response()
+class SubtractView(AccountMixin, generics.GenericAPIView):
+    """Place a hold"""
 
-    def post(self, request, format=None):
-        return Response()
+    def transaction(self, instance, data, resp):
+        amount = Decimal(data['amount'])
+        if instance.balance - instance.hold - amount < 0:
+            resp['status'] = status.HTTP_400_BAD_REQUEST
+            resp['result'] = False
+            resp['addition'] = self.customer_serializer_class(instance=instance).data
+            resp['description'] = {
+                'errors': ['Not enough funds']
+            }
+            return Response(resp, status=resp['status'])
+
+        instance.hold += amount
+        instance.save()
+
+        resp['status'] = status.HTTP_200_OK
+        resp['result'] = True
+        resp['addition'] = self.customer_serializer_class(instance=instance).data
+        return Response(resp, status=resp['status'])
+
+
+class StatusView(AccountMixin, generics.GenericAPIView):
+    """Balance Status"""
+
+    serializer_class = InfoSerializer
 
 
 @api_view()
 def ping(request):
-    return Response({"message": "pong"})
+    data = OrderedDict([
+        ('status', status.HTTP_200_OK),
+        ('result', True),
+        ('addition', {'operation': 'ping'}),
+        ('description', {})
+    ])
+    return Response(data, status=data['status'])
 
 
 add_view = AddView.as_view()
