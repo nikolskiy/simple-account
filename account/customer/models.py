0a58ab11f8fc708951941afede67ff87ba535501
Denis <fsdvn1@gmail.com>
2019-12-26 00:22:59 -0800

    Customer model was added.

--- account/customer/models.py
+++ account/customer/models.py
@@ -1,3 +1,13 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    uuid = models.UUIDField(default=uuid4, editable=False)
+    name = models.CharField(max_length=500)
+
+    # values up to 999999.99
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
+    hold = models.DecimalField(max_digits=8, decimal_places=2)
+
+    status = models.BooleanField()
