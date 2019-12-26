39e701f4e39160e64ae772900ed5f2fe8c9eb1f9
Denis <fsdvn1@gmail.com>
2019-12-26 04:35:39 -0800

    ping, add, subtract, status views were added.

--- account/customer/urls.py
+++ account/customer/urls.py
@@ -1,12 +1,16 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import CustomerViewSet
+from . import views
 
 
 router = routers.DefaultRouter()
-router.register('customers', CustomerViewSet)
+router.register('customers', views.CustomerViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('ping/', views.ping),
+    path('add/', views.add_view),
+    path('subtract/', views.subtract_view),
+    path('status/', views.status_view)
 ]
--- account/customer/views.py
+++ account/customer/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, views
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Customer
 from .serializers import CustomerSerializer
 
@@ -7,3 +9,37 @@ class CustomerViewSet(viewsets.ModelViewSet):
     """Manage customer records"""
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+
+class AddView(views.APIView):
+    def get(self, request, format=None):
+        return Response()
+
+    def post(self, request, format=None):
+        return Response()
+
+
+class SubtractView(views.APIView):
+    def get(self, request, format=None):
+        return Response()
+
+    def post(self, request, format=None):
+        return Response()
+
+
+class StatusView(views.APIView):
+    def get(self, request, format=None):
+        return Response()
+
+    def post(self, request, format=None):
+        return Response()
+
+
+@api_view()
+def ping(request):
+    return Response({"message": "pong"})
+
+
+add_view = AddView.as_view()
+subtract_view = SubtractView.as_view()
+status_view = StatusView.as_view()
