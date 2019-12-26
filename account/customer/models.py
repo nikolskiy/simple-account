097c04756f5ebf0a7e9aa548afbfb89649522d85
Denis <fsdvn1@gmail.com>
2019-12-26 01:15:04 -0800

    Customer unique id was set to uuid field.
    That way there is no confusion which id to use to reference a customer.

--- account/customer/models.py
+++ account/customer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Customer(models.Model):
-    uuid = models.UUIDField(default=uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=500)
 
     # values up to 999999.99
